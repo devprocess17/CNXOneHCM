39a810f031a3f3e2561877fdc90435256eda7fce
Captor-Dev <Professional-Dev@outlook.com>
2025-06-11 21:15:53 +0100

    width change

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -37,7 +37,7 @@ export default function Navigation({ onDemoClick }: NavigationProps) {
         isScrolled ? 'bg-white shadow-lg' : 'bg-transparent'
       }`}
     >
-      <div className="max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-[1920px] px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 md:h-20">
           <div className="flex-shrink-0">
             <a href="https://cnxlink.cnx4-0.com" className="block">
